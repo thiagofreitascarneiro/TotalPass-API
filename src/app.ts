470785a4fe924bd7d4206534c1c064fd85aae207
Thiago <thiagofreitascarneiro@hotmail.com>
2023-07-12 20:38:16 -0300

    creating use case and repository Pattern

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-
-prisma.user.create({
-    data: {
-        name: 'Thiago Carneiro',
-        email: 'thiago@gmail.com',
-    }
-});
+app.register(appRoutes);
