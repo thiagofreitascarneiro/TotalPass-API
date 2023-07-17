c119fc17ccc7b8e924e3f0ea8e4ac700ec1a5418
Thiago <thiagofreitascarneiro@hotmail.com>
2023-07-17 16:08:57 -0300

    creating error cases

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma';
 import { UsersRepository } from '@/repositories/users-repository';
 import { hash } from 'bcryptjs';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 
 interface RegisterUseCaseRequest {
    name: string;
@@ -17,14 +18,10 @@ export class RegisterUseCase {
     }:RegisterUseCaseRequest) {
         const password_hash = await hash(password, 6);
     
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email,
-            }
-        });
+        const userWithSameEmail = await this.usersRepository.findByEmail(email);
     
         if(userWithSameEmail) {
-            throw new Error('E-mail already exists.');
+            throw new UserAlreadyExistsError();
         }
     
         await this.usersRepository.create({
